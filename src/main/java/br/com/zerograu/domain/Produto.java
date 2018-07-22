015ca29b33b431c3e493e703d9b53ef875ab20b4
matheusmr <matheusmr@ciandt.com>
2018-07-21 21:53:12 -0300

    Initial commit

---
+++ src/main/java/br/com/zerograu/domain/Produto.java
@@ -0,0 +1,62 @@
+package br.com.zerograu.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id_produto;
+
+    @Column
+    private String desc_produto;
+
+    @Column
+    private double preco_compra;
+
+    @Column
+    private double preco_venda;
+
+    @Column
+    private int qtd_estoque;
+
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(int id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public String getDesc_produto() {
+        return desc_produto;
+    }
+
+    public void setDesc_produto(String desc_produto) {
+        this.desc_produto = desc_produto;
+    }
+
+    public double getPreco_compra() {
+        return preco_compra;
+    }
+
+    public void setPreco_compra(double preco_compra) {
+        this.preco_compra = preco_compra;
+    }
+
+    public double getPreco_venda() {
+        return preco_venda;
+    }
+
+    public void setPreco_venda(double preco_venda) {
+        this.preco_venda = preco_venda;
+    }
+
+    public int getQtd_estoque() {
+        return qtd_estoque;
+    }
+
+    public void setQtd_estoque(int qtd_estoque) {
+        this.qtd_estoque = qtd_estoque;
+    }
+}
